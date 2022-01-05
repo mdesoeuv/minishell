831f3d1bd32cc918891e5c812e27f6df18ce6d87
mdesoeuv <mdesoeuv@student.42lyon.fr>
2022-01-05 11:47:32 +0100

    t_bool enum + basic struct cmd

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:00:41 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/01/04 17:40:43 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/01/05 11:47:00 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,20 @@
 # include <dirent.h>
 # include <termios.h>
 # include "../libft/libft.h"
+
+typedef enum e_bool
+{
+	false,
+	true
+}	t_bool;
+
+typedef struct s_cmd
+{
+	char	*cmd_path;
+	char	**cmd_arg;
+	char	*cmd_in;
+	char	*cmd_out;
+}	t_cmd;
 
 void	print_working_directory(void);
 
