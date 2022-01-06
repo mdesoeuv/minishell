8ae7f20241e35c1b6a41f915d5f5b8b79fcf7770
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-01-06 19:27:14 +0100

    update parsings lists .h

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:00:41 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/01/06 19:13:59 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/06 19:25:41 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,19 +46,26 @@ typedef struct s_cmd
 	char	*cmd_out;
 }	t_cmd;
 
-typedef struct s_list_colons
+typedef struct s_list_semicolons
 {
-	struct s_list_pipes		*content;
-	struct s_list_colons	*next;
-}	t_list_colons;
+	struct s_list_pipes			*pipes_list;
+	struct s_list_semicolons	*next;
+}	t_list_semicolons;
 
 typedef struct s_list_pipes
 {
-	char				**name;
-	char				**filein;
-	char				**fileout;
-	struct s_list_pipes	*next;
+	char					**name;
+	struct s_list_pipe_args	**args;
+	char					**filein;
+	char					**fileout;
+	struct s_list_pipes		*next;
 }	t_list_pipes;
+
+typedef struct s_list_pipe_args
+{
+	char					**name;
+	struct s_list_pipe_args	*next;
+}	t_list_pipe_args;
 
 void	print_working_directory(void);
 char	*return_working_directory(void);
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,11 +6,20 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 19:06:14 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/06 19:07:05 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/06 19:16:57 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_parsing(char *line)
+#include "minishell.h"
+
+t_list_colons	ft_parsing(char *line)
 {
-	
+	int	i;
+	t_list_colons colons_list
+
+	i = 0;
+	while (line[i])
+	{
+		
+	}
 }
