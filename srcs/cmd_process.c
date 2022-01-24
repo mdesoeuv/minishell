0c94c5d31eabc2d05e6b7f814c9470edc9affc42
Mehdi <mehdi.desoeuvre@gmail.com>
2022-01-24 17:56:43 +0100

    builtin execution in fork only when piped

--- srcs/cmd_process.c
+++ srcs/cmd_process.c
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 14:50:13 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/01/24 16:24:19 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/01/24 17:55:17 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,14 +85,56 @@ void	eval_child_status(int child_status)
 	}
 }
 
-/*	returns 0 if not built-in, else returns function's index */
-
-int	execute_if_built_in(t_shell *shell, t_list_pipes *pipe_lst)
+int	is_it_builtin(t_list_pipes *pipe_lst)
 {
-	if (!pipe_lst->command[0])
-		return (-999);
-	if (pipe_lst->to_execute == 0)
-		return (-100);
+	if (ft_strcmp(pipe_lst->command[0], "pwd") == 0)
+		return (1);
+	else if (ft_strcmp(pipe_lst->command[0], "cd") == 0)
+		return (1);
+	else if (ft_strcmp(pipe_lst->command[0], "echo") == 0)
+		return (1);
+	else if (ft_strcmp(pipe_lst->command[0], "export") == 0)
+		return (1);
+	else if (ft_strcmp(pipe_lst->command[0], "unset") == 0)
+		return (1);
+	else if (ft_strcmp(pipe_lst->command[0], "env") == 0)
+		return (1);
+	else if (ft_strcmp(pipe_lst->command[0], "exit") == 0)
+		return (1);
+	else
+		return (0);
+}
+
+int	exec_builtin_fork(t_shell *shell, t_list_pipes *pipe_lst)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+		ft_free("minishell: fork error\n", shell, 1, 1);
+	else if (pid == 0)
+	{
+		if (ft_strcmp(pipe_lst->command[0], "pwd") == 0)
+			print_working_directory();
+		else if (ft_strcmp(pipe_lst->command[0], "cd") == 0)
+			change_directory(shell, pipe_lst->command[1]);
+		else if (ft_strcmp(pipe_lst->command[0], "echo") == 0)
+			ft_echo(shell);
+		else if (ft_strcmp(pipe_lst->command[0], "export") == 0)
+			ft_export(shell, pipe_lst->command[1]);
+		else if (ft_strcmp(pipe_lst->command[0], "unset") == 0)
+			ft_unset(shell, pipe_lst->command[1]);
+		else if (ft_strcmp(pipe_lst->command[0], "env") == 0)
+			ft_env(shell, pipe_lst->command[1]);
+		else if (ft_strcmp(pipe_lst->command[0], "exit") == 0)
+			shell->is_exit = ft_exit(shell);
+		exit(0);
+	}
+	return (0);
+}
+
+int	exec_builtin(t_shell *shell, t_list_pipes *pipe_lst)
+{
 	if (ft_strcmp(pipe_lst->command[0], "pwd") == 0)
 		return (print_working_directory());
 	else if (ft_strcmp(pipe_lst->command[0], "cd") == 0)
@@ -105,8 +147,27 @@ int	execute_if_built_in(t_shell *shell, t_list_pipes *pipe_lst)
 		return (ft_unset(shell, pipe_lst->command[1]));
 	else if (ft_strcmp(pipe_lst->command[0], "env") == 0)
 		return (ft_env(shell, pipe_lst->command[1]));
-	else if (ft_strcmp(shell->list_start->command[0], "exit") == 0)
+	else if (ft_strcmp(pipe_lst->command[0], "exit") == 0)
+	{
 		shell->is_exit = ft_exit(shell);
+		return (0);
+	}
+	return (0);
+}
+
+int	execute_if_built_in(t_shell *shell, t_list_pipes *pipe_lst)
+{
+	if (!pipe_lst->command[0])
+		return (-999);
+	if (pipe_lst->to_execute == 0)
+		return (-100);
+	if (is_it_builtin(pipe_lst) == 1)
+	{
+		if (shell->cmd_nbr > 1)
+			return (exec_builtin_fork(shell, pipe_lst));
+		else
+			return (exec_builtin(shell, pipe_lst));
+	}
 	return (-100);
 }
 
