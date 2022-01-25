0065c3994b1ac1bad0b25a89ba2a144d8fec09e3
Victor Chevillotte <victor.chevillotte@gmail.com>
2022-01-25 13:33:17 +0100

    fixed echo -n

--- srcs/built_in_echo.c
+++ srcs/built_in_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   built_in_echo.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 14:29:22 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/17 09:45:00 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/01/25 13:33:00 by vchevill         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,29 @@ int	ft_echo(t_shell *shell)
 {
 	int		i;
 	int		option_n;
+	int		j;
 
 	i = 1;
+	j = 1;
 	option_n = 0;
 	if (nb_args(shell->list_start->command) > 1)
 	{
-		while (shell->list_start->command[i]
-			&& ft_strcmp(shell->list_start->command[i], "-n") == 0)
+		if (shell->list_start->command[i]
+			&& ft_strncmp(shell->list_start->command[i], "-n", 2) == 0)
 		{
-			option_n = 1;
-			i++;
+			while (shell->list_start->command[i][j]
+				|| shell->list_start->command[i][j] != ' ')
+			{
+				if (shell->list_start->command[i][j] != 'n')
+					break ;
+				j++;
+			}
+			if (!shell->list_start->command[i][j]
+				|| shell->list_start->command[i][j] == ' ')
+			{
+				option_n = 1;
+				i++;	
+			}
 		}
 		while (shell->list_start->command[i])
 		{
