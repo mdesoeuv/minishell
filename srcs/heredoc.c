9cf80716f5bbcf09966bf49d5575cc16bd085a65
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-01-26 15:58:10 +0100

    fixed ctrl-c

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 13:22:11 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/01/26 15:49:23 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/01/26 15:57:50 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ int	heredoc_process(t_shell *shell, char *ending_line, int pipe_fd[2])
 			ft_free("minishell: memory allocation error\n", shell, 1, 1);
 		line = readline("> ");
 	}
-	signal(SIGINT, &sig_int);
 	write(pipe_fd[1], total_line, ft_strlen(total_line));
 	free(line);
 	free(total_line);
@@ -73,6 +72,7 @@ int	here_doc_v2(t_shell *shell, t_list_pipes *pipe_lst)
 	pid = fork();
 	if (pid < 0)
 		ft_free("minishell: fork error\n", shell, 1, 1);
+	signal(SIGINT, SIG_IGN);
 	if (pid == 0)
 	{
 		heredoc_process(shell, ending_line, pipe_fd);
