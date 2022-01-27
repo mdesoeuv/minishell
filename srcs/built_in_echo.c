006eff28e8e87e191638f8e40551a0435c72458c
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-01-27 11:37:16 +0100

    norme echo ok

--- srcs/built_in_echo.c
+++ srcs/built_in_echo.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 14:29:22 by vchevill          #+#    #+#             */
-/*   Updated: 2022/01/25 13:33:00 by vchevill         ###   ########lyon.fr   */
+/*   Updated: 2022/01/27 11:36:07 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,28 @@ static int	nb_args(char **args)
 	return (size);
 }
 
+void	ft_echo_2(t_shell *shell, int *i, int *j, int *option_n)
+{
+	if (shell->list_start->command[*i]
+		&& ft_strncmp(shell->list_start->command[*i], "-n", 2) == 0)
+	{
+		while (shell->list_start->command[*i][*j]
+			|| shell->list_start->command[*i][*j] != ' ')
+		{
+			if (shell->list_start->command[*i][*j] != 'n')
+				break ;
+			(*j)++;
+		}
+		if (!shell->list_start->command[*i][*j]
+			|| shell->list_start->command[*i][*j] == ' ')
+		{
+			*option_n = 1;
+			(*i)++;
+		}
+	}
+}
+
+
 int	ft_echo(t_shell *shell)
 {
 	int		i;
@@ -33,23 +55,7 @@ int	ft_echo(t_shell *shell)
 	option_n = 0;
 	if (nb_args(shell->list_start->command) > 1)
 	{
-		if (shell->list_start->command[i]
-			&& ft_strncmp(shell->list_start->command[i], "-n", 2) == 0)
-		{
-			while (shell->list_start->command[i][j]
-				|| shell->list_start->command[i][j] != ' ')
-			{
-				if (shell->list_start->command[i][j] != 'n')
-					break ;
-				j++;
-			}
-			if (!shell->list_start->command[i][j]
-				|| shell->list_start->command[i][j] == ' ')
-			{
-				option_n = 1;
-				i++;	
-			}
-		}
+		ft_echo_2(shell, &i, &j, &option_n);
 		while (shell->list_start->command[i])
 		{
 			ft_putstr(shell->list_start->command[i]);
